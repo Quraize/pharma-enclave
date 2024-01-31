d5868e6e5a937cb978b0da549566023dad8e48f5
Iqrma <iqrmaai@gmail.com>
2024-01-31 20:13:02 +0500

    db is connected and running. envVariables are set.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
+const port = process.env.PORT || 3000;
 const app = express();
+dotenv.config();
+connectDB();
 
-app.listen(3000, ()=>{
-    console.log('server listening on the port 3000!');
+app.listen(port, ()=>{
+    console.log(`Server is listening on the port ${port}`);
 })
