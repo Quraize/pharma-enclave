0d386401520c9c2bc30694eb5b482a5e391b25d0
Iqrma <iqrmaai@gmail.com>
2024-02-01 12:46:07 +0500

    created and tested api routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 import connectDB from './config/db.js';
 
 const port = process.env.PORT || 3000;
@@ -10,3 +11,5 @@ connectDB();
 app.listen(port, ()=>{
     console.log(`Server is listening on the port ${port}`);
 })
+
+app.use('/api/user', userRouter);
