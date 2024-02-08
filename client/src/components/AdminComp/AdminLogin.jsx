2b5ebe52df3eb10ee042e7331eb8d694e3977d16
Iqrma <iqrmaai@gmail.com>
2024-02-08 17:46:48 +0500

    included redux persist to manage user state.

--- client/src/components/AdminComp/AdminLogin.jsx
+++ client/src/components/AdminComp/AdminLogin.jsx
@@ -1,95 +1,102 @@
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
-import './AdminLoginStyles.css';
-import {signInStart, signInSuccess, signInFailure} from '../../redux/admin/adminSlice';
+import "./AdminLoginStyles.css";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../../redux/admin/adminSlice";
 import { useDispatch, useSelector } from "react-redux";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 
 export default function AdminLogin() {
   const [formData, setformData] = useState({});
-  const { loading, error } = useSelector((state) => state.admin)
+  const { loading, error } = useSelector((state) => state.admin);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleChange = (e) => {
-    setformData({...formData, [e.target.id]: e.target.value})
+    setformData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('http://localhost:3000/admin/authadmin', {
-        method: 'POST',
+      const res = await fetch("http://localhost:3000/admin/authadmin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(signInFailure(data));
         return;
       }
       dispatch(signInSuccess(data));
-      navigate('/admin')
+      navigate("/admin");
     } catch (error) {
       dispatch(signInFailure(error));
     }
-  }
+  };
 
-    useEffect(() => {
-        AOS.init({
-          duration : 1300,
-          easing: 'ease-in-out-back'
-        });
-      }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1300,
+      easing: "ease-in-out-back",
+    });
+  }, []);
   return (
     <div className="adminlogin-main-sec">
-        <div>
-            <h1 className="adminlogin-self-heading" data-aos='fade-right'>PharmaEnclave</h1>
-            <p className="adminlogin-self-para" data-aos='fade-left'>Admin Authentication</p>
-        </div>
-      
-      <form onSubmit={handleSubmit} className='form-self'>
-        <label className="email-label-self" data-aos='slide-right'>
+      <div>
+        <h1 className="adminlogin-self-heading" data-aos="fade-right">
+          PharmaEnclave
+        </h1>
+        <p className="adminlogin-self-para" data-aos="fade-left">
+          Admin Authentication
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit} className="form-self">
+        <label className="email-label-self" data-aos="slide-right">
           Email
-          </label>
+        </label>
         <input
-          type='email'
-          placeholder='Email'
-          id='email'
-          className='signin-email-pacleholder'
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="signin-email-pacleholder"
           onChange={handleChange}
-          data-aos='slide-left'
+          data-aos="slide-left"
         />
-        <label className="password-label-self" data-aos='slide-right'>
+        <label className="password-label-self" data-aos="slide-right">
           Password
-          </label>
+        </label>
         <input
-          type='password'
-          placeholder='Password'
-          id='password'
-          className='signin-password-placeholder'
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="signin-password-placeholder"
           onChange={handleChange}
-          data-aos='slide-left'
+          data-aos="slide-left"
         />
         <Button
           variant="secondary"
           disabled={loading}
-          className='button-self'
-          type='submit'
-          data-aos='slide-up'
+          className="button-self"
+          type="submit"
+          data-aos="slide-up"
         >
-          {loading ? 'Loading...' : 'Sign In'}
+          {loading ? "Loading..." : "Sign In"}
         </Button>
       </form>
       <p className="adminlogin-error-handler">
-        {error ? error.message || 'Somthing went wrong' : ''}
-        </p>
+        {error ? error.message || "Somthing went wrong" : ""}
+      </p>
     </div>
   );
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers } from '@reduxjs/toolkit';
 import adminReducer from './admin/adminSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ admin: adminReducer });
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {admin: adminReducer},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
         serializableCheck: false,
-    }),
-});
+      }),
+  });
+  
+  export const persistor = persistStore(store);
